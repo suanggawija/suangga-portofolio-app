74626e2d9a4e6385593f664b05ad5c2909fee217
suanggawija <suanggawija@gmail.com>
2025-06-18 16:47:55 +0800

    feat: Change contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,7 +1,23 @@
+"use client";
+import { ButtonSecondary } from "@/components/Button";
+import { RoundedBlurBackground } from "@/components/RoundedBlur";
+import { H1 } from "@/components/Text";
 import React from "react";
+import { motion } from "motion/react";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <>
+      <section className="w-full min-h-[500px] flex flex-col gap-4 justify-center items-center relative px-[80px] relative pt-[80px]">
+        <H1 title="Kontak" />
+        <div className="flex gap-2">
+          <ButtonSecondary href="" title="Email" delay={0.3} />
+          <ButtonSecondary href="" title="WhatsApp" delay={0.5} />
+        </div>
+        <RoundedBlurBackground />
+      </section>
+    </>
+  );
 };
 
 export default Contact;
