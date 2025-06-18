e1d69f7903a1b02911dce785f77a04ae4c2b49c9
suanggawija <suanggawija@gmail.com>
2025-06-18 16:48:11 +0800

    feat: Change portofolio page

--- app/portofolio/page.tsx
+++ app/portofolio/page.tsx
@@ -10,13 +10,13 @@ import { motion } from "motion/react";
 const Portofolio = () => {
   return (
     <>
-      <section className="w-full min-h-screen flex flex-col gap-4 justify-center items-center relative px-[80px] mt-[80px]">
+      <section className="w-full min-h-screen flex flex-col gap-4 justify-center items-center relative px-[80px] mt-[120px]">
         <H1 title="Portofolio" />
         <P
           title="Project Portofolio Suangga yang mencangkup Website, Data Analisis, UI UX dan Grafix Desain"
           delay={0.5}
         />
-        <div className="w-full grid grid-cols-2 gap-4 ">
+        <div className="w-full grid grid-cols-2 gap-4 mt-6 mb-[80px]">
           {PortofolioData.map((porto, index) => (
             <PortofolioCart
               key={index}
@@ -27,9 +27,16 @@ const Portofolio = () => {
               delay={index % 2 == 0 ? 0.4 : 0.8}
             />
           ))}
-        </div>
-        <div className="flex w-full justify-end mt-3">
-          <LinkUnderline title=" Lihat Portofolio Lainnya" href="/portofolio" />
+          {PortofolioData.map((porto, index) => (
+            <PortofolioCart
+              key={index}
+              image={porto.images}
+              title={porto.title}
+              desc={porto.desc}
+              tags={porto.tags}
+              delay={index % 2 == 0 ? 0.4 : 0.8}
+            />
+          ))}
         </div>
         <RoundedBlurBackground />
       </section>
